580def7b52ffc725e5878c7e911d37d10742d44a
Садик Халилов <halilovsadik4@gmail.com>
2025-06-20 15:47:35 +0000

    посмотри я всё сделал как ты сказал

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -28,7 +28,7 @@ export default function HeroSection() {
               </Button>
             </div>
           </div>
-          <div className="hidden md:block relative aspect-square max-w-md mx-auto md:max-w-none md:mx-0 bg-background">
+          <div className="hidden md:block relative aspect-square max-w-md mx-auto md:max-w-none md:mx-0">
             <Image
               src="/logo-hero.png" 
               alt={`Логотип ${APP_NAME}`}
