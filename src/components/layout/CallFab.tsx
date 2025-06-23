1519437293560f5e8cc22541ecb3d2949037c8f1
Садик Халилов <halilovsadik4@gmail.com>
2025-06-23 03:23:37 +0000

    КНОПКА СВЯЗИ ЗАКРЫВАЕТЗНАЧЁК КНОПКЕ войти

--- src/components/layout/CallFab.tsx
+++ src/components/layout/CallFab.tsx
@@ -12,7 +12,7 @@ export default function CallFab() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="fixed bottom-6 right-6 z-50">
+    <div className="fixed bottom-24 right-6 z-50 md:bottom-6">
       <div className="relative">
         {/* Pop-up window for contact options */}
         <div
